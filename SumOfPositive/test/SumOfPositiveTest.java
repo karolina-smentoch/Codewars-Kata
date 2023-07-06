10ba08c0a581eb625643eb60788f728fe1ece959
Karolina Smentoch <karolinasmentoch@gmail.com>
2023-07-06 21:34:17 +0200

    Sum of positive and square of squares added

---
+++ SumOfPositive/test/SumOfPositiveTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SumOfPositiveTest {
+    @Test
+    public void testSomething() {
+        assertEquals(15, SumOfPositive.sum(new int[]{1,2,3,4,5}));
+        assertEquals(13, SumOfPositive.sum(new int[]{1,-2,3,4,5}));
+        assertEquals(0, SumOfPositive.sum(new int[]{}));
+        assertEquals(0, SumOfPositive.sum(new int[]{-1,-2,-3,-4,-5}));
+        assertEquals(9, SumOfPositive.sum(new int[]{-1,2,3,4,-5}));
+    }
+
+}
