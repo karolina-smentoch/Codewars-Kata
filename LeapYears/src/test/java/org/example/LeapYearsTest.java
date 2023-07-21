ee705f8e73d78bfc58b4b659658959cc1e01ecaf
Karolina Smentoch <karolinasmentoch@gmail.com>
2023-07-21 18:12:16 +0200

    LeapYears

---
+++ LeapYears/src/test/java/org/example/LeapYearsTest.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LeapYearsTest
+{
+
+    @Test
+    public void testYear2020()
+    {
+        assertEquals(true, LeapYears.isLeapYear(2020));
+    }
+
+    @Test
+    public void testYear2000()
+    {
+        assertEquals(true, LeapYears.isLeapYear(2000));
+    }
+
+    @Test
+    public void testYear2015()
+    {
+        assertEquals(false, LeapYears.isLeapYear(2015));
+    }
+
+    @Test
+    public void testYear2100()
+    {
+        assertEquals(false, LeapYears.isLeapYear(2100));
+    }
+}
+
