b6d734383f86ba71cdbfeb706f55896f5d6c8d2a
Karolina Smentoch <karolinasmentoch@gmail.com>
2023-07-31 20:39:18 +0200

    OppositeNumber

---
+++ OppositeNumber/src/main/java/org/example/OppositeNumber.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * Very simple, given an integer or a floating-point number, find its opposite.
+ * <p>
+ * Examples:
+ * <p>
+ * 1: -1
+ * 14: -14
+ * -34: 34
+ */
+public class OppositeNumber {
+    public static int opposite(int number) {
+        return number * (-1);
+    }
+}
---
+++ OppositeNumber/src/test/java/org/example/OppositeNumberTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class OppositeNumberTest {
+    @Test
+    public void tests() {
+        assertEquals(-1, OppositeNumber.opposite(1));
+    }
+}
